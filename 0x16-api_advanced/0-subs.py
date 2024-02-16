d41b643e12b949482cf21edf4ec96c5a897f08b9
noahwekesa <noahwekesa0@gmail.com>
2024-02-17 01:11:13 +0300

    fixing comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Return the no. of subs in a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
