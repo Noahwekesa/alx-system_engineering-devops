34cef9509269be70b9360709eb841fb1bfb2b1cb
noahwekesa <noahwekesa0@gmail.com>
2024-02-17 01:25:38 +0300

    fixing bug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-""" Query Reddit API and returns the number of subs """
+""" Query calls Reddit API and returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the no. of subs in a given subreddit"""
+    """returns number of subs given a subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
+    headers = {"User-Agent": "Mozilla/5 (by /u/COOLDOWNYOURPACE)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    if response.status_code != 200:
         return 0
+    num_subs = response.json().get("data").get("subscribers")
+    return num_subs
