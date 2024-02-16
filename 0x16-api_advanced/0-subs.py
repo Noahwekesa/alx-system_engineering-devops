08622fef087223d54f095297d99588ea376fc65f
noahwekesa <noahwekesa0@gmail.com>
2024-02-17 01:06:08 +0300

    how manay subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,8 +4,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "i will leave it black"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
