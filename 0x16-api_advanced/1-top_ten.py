f11ea02fdfc2b908ab791c961823d2209f61850a
noahwekesa <noahwekesa0@gmail.com>
2024-02-17 01:27:29 +0300

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and prints the titles of the first 10 hot posts listed
-"""
-
+""" calls the Reddit API and prints the titles of the first 10 hot posts """
 import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        for post in posts[:10]:
-            title = post["data"]["title"]
-            print(title)
+    """returns the first 10 hot posts given a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
+    payload = {"limit": "10"}
+    response = requests.get(url, headers=headers, params=payload, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
     else:
-        print(None)
-
-    if response.status_code != 200 and response.status_code != 302:
-        print("Invalid subreddit or API error.")
+        hot_list = response.json().get("data").get("children")
+        hot_titles = [post.get("data").get("title") for post in hot_list]
+        print(*hot_titles, sep="\n")
