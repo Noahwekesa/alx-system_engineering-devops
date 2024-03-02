06c6b8306f51dd4bdc35cc3d147979ed027d75f1
Wekesa <57433233+Noahwekesa@users.noreply.github.com>
2024-03-02 15:09:21 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# fixes corrupted file with extension phpp
-exec { 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php':
-  path => '/usr/bin:/usr/sbin:/bin',
-}
--> exec {'restart service':
-  command => '/usr/sbin/service apache2 restart',
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,3 +0,0 @@
-# Web stack debugging #3
-
-- Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet
