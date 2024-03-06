e64a33d3d01124f1aa1f7cd33dc678f66b94b6a7
noahwekesa <noahwekesa0@gmail.com>
2024-03-06 13:54:20 +0300

    fixed pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,12 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
     payload = {"limit": "10"}
-    response = requests.get(url, headers=headers, params=payload, allow_redirects=False)
+    response = requests.get(
+        url,
+        headers=headers,
+        params=payload,
+        allow_redirects=False,
+    )
     if response.status_code != 200:
         print("None")
     else:
