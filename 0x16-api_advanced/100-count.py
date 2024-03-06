ca4c0d90969b4f0901b95decf1cedce195ee6cf6
noahwekesa <noahwekesa0@gmail.com>
2024-03-06 13:51:38 +0300

    fixed pycodestylee

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,7 +9,12 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (/u/COOLDOWNYOURPACE)"}
     payload = {"limit": "100", "after": after}
-    hot = requests.get(url, headers=headers, params=payload, allow_redirects=False)
+    hot = requests.get(
+        url,
+        headers=headers,
+        params=payload,
+        allow_redirects=False,
+    )
     if hot.status_code == 200:
         posts = hot.json().get("data").get("children")
         hot_list += [post.get("data").get("title") for post in posts]
@@ -23,7 +28,12 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
             rt = []
             for word in word_list_low:
                 num = len(
-                    list(filter(lambda hot_w: hot_w.lower() == word.lower(), hot_words))
+                    list(
+                        filter(
+                            lambda hot_w: hot_w.lower() == word.lower(),
+                            hot_words,
+                        ),
+                    )
                 )
                 if num != 0:
                     rt.append([word, num])
