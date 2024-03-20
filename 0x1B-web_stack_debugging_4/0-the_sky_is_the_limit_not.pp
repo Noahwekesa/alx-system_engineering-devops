8c39e5c10bc15dc750bea526fd02f77540a4b2e3
noahwekesa <noahwekesa0@gmail.com>
2024-03-20 22:51:22 +0300

    fix issue

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,4 @@
-# limit fix
-exec { 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx':
-  path => '/usr/bin:/usr/sbin:/bin',
-}
--> exec {'restart service':
-  command => '/usr/sbin/service nginx restart',
-}
+exec { 'fix_limit':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  =>  "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3072\"/g' /etc/default/nginx; sudo service nginx restart",
+  provider =>  'shell'}
